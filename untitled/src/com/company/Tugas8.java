62e793b7838df28de806dd0918c87f1d5a19b904
saniwisnu <asc.kaka21@gmail.com>
2015-03-25 15:49:26 +0700

    Revert "Revert "Tugas""
    This reverts commit b6f3a47197e1b37555b083773b0f78a1da834e8d.

---
+++ untitled/src/com/company/Tugas8.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by VAIO on 3/25/2015.
+ */
+public class Tugas8 {
+    public static void main(String[] args) {
+
+        int awal = 1;
+        int akhir = 110;
+
+        for(awal=1; awal<=110; awal++){
+            System.out.print(awal + " ");
+            if (awal%11==0){
+                System.out.print("\n");
+            }
+        }
+    }
+}
+
